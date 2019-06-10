6145ada07855b7cdf2618e5f95737a4989c68276
Dror Tirosh <dror@tabookey.com>
2019-06-10 18:12:43 +0300

    solidity separate project.

---
+++ solidity/test/test_contract.js
@@ -0,0 +1,18 @@
+var Contract = artifacts.require("./Contract.sol");
+
+contract('Contract', function (accounts) {
+
+    it("should work", async function () {
+    	let contract = await Contract.deployed()
+    	let res = await contract.emitMessage("Hello", 1)
+        let event = res.logs[0];
+
+        assert.equal(event.args.message,"Hello")
+        assert.equal(event.args.value, 1)
+
+    	let res2 = await contract.getData()
+        assert.equal(res2["0"],2)
+        assert.equal(res2["1"], 3)
+    });
+
+});
