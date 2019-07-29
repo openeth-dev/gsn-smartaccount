b4053acaf889620c6e797da91ec7acb8978d9258
Alex Forshtat <alex@tabookey.com>
2019-07-29 13:18:56 +0300

    Remove unused 'Contract.sol'

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -1,16 +1,15 @@
-const Contract = artifacts.require("./Contract.sol");
 const Vault = artifacts.require("./Vault.sol");
 const VaultFactory = artifacts.require("./VaultFactory.sol");
 const Gatekeeper = artifacts.require("./Gatekeeper.sol");
 
 
 module.exports = async function (deployer) {
-    await deployer.deploy(Contract);
+    await deployer.deploy(VaultFactory, {gas: 8000000});
+    // I think there is a bug in truffle, trying to deploy Gatekeeper first causes an error for no reason
     console.log("Deploying Gatekeeper");
     let gatekeeper = await deployer.deploy(Gatekeeper);
-	console.log("Gatekeeper",Gatekeeper.address);
+    console.log("Gatekeeper", Gatekeeper.address);
     console.log("Deploying Vault");
     await deployer.deploy(Vault, gatekeeper.address);
-    console.log("Vault",Vault.address);
-    await deployer.deploy(VaultFactory,{gas:8000000});
+    console.log("Vault", Vault.address);
 };
