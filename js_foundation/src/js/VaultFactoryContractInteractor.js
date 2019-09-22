214da3af3a0a1479c1ab93a662a06a09f7097a12
Alex Forshtat <alex@tabookey.com>
2019-09-22 18:06:00 +0300

    Add check for node module to avoid reinstalling from Gradle (!Install manually to update!)

---
+++ js_foundation/src/js/VaultFactoryContractInteractor.js
@@ -0,0 +1,8 @@
+
+class VaultFactoryContractInteractor {
+    deployNewGatekeeper(){
+        return {gatekeeper: "0x12345678901234567890"}
+    }
+}
+
+module.exports = VaultFactoryContractInteractor;
