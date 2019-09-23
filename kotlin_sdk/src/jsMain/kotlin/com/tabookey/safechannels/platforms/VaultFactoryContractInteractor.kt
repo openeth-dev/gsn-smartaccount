95a9370e0ecb7b9b236d7a3c17babc0a17410767
Alex Forshtat <alex@tabookey.com>
2019-09-23 14:06:18 +0300

    Make Mocha tests run from within Idea's Mocha plug-in
    Also, made 'skipIfNodeModuleExists' accept arrays.
    TODO: something sane instead of the mess my ad-hoc build system is :-(

--- kotlin_sdk/src/jsMain/kotlin/com/tabookey/safechannels/platforms/VaultFactoryContractInteractor.kt
+++ kotlin_sdk/src/jsMain/kotlin/com/tabookey/safechannels/platforms/VaultFactoryContractInteractor.kt
@@ -2,4 +2,8 @@ package com.tabookey.safechannels.platforms
 
 actual external class VaultFactoryContractInteractor {
     actual suspend fun deployNewGatekeeper(): Response
+
+    actual companion object {
+        actual suspend fun connect(): VaultFactoryContractInteractor
+    }
 }
