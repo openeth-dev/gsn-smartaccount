6eadb88a39a800ca53dbe9ce46da0cd54fe4539d
Alex Forshtat <alex@tabookey.com>
2019-11-25 13:07:16 +0200

    Add 'Placeholder' imports of RelayHub, GsnForwarder

---
+++ solidity/contracts/tests/Placeholders.sol
@@ -0,0 +1,8 @@
+// Fake DAI contract for local testing.  Do not deploy.
+
+pragma solidity >=0.4.0 <0.6.0;
+
+import "tabookey-gasless/contracts/RelayHub.sol";
+import "gsn-sponsor/contracts/GsnForwarder.sol";
+
+contract Placeholders {}
