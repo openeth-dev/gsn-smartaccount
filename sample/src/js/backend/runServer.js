1a8a74e0daa0d45ada9002cc7a298c3557cf84b9
shahafn <shahaf@tabookey.com>
2019-12-10 19:59:01 +0200

    Backend fix (#41)
    * Rename
    
    * Fixing default handler in webserver
    
    Adding --dev option to runServer.js
    
    * removing comment

--- sample/src/js/backend/runServer.js
+++ sample/src/js/backend/runServer.js
@@ -1,13 +1,14 @@
 /*
   Run with:
-  node -r esm <path_to_script>/runServer.js <port>
+  node -r esm <path_to_script>/runServer.js <port> [<devMode>]
  */
 
 import Webserver from './Webserver'
 import { Backend } from './Backend'
-
 import Wallet from 'ethereumjs-wallet'
 import SMSmock from '../mocks/SMS.mock'
+import { LoginTicket } from 'google-auth-library/build/src/auth/loginticket'
+import ticket from '../../../test/backend/ticket.json'
 
 function newEphemeralKeypair () {
   const a = Wallet.generate()
@@ -15,6 +16,22 @@ function newEphemeralKeypair () {
     privateKey: a.privKey,
     address: '0x' + a.getAddress().toString('hex')
   }
+}
+
+function hookBackend (backend) {
+  backend.gclient._orig_verifyIdToken = backend.gclient.verifyIdToken
+  const verifyFn = async function ({ idToken, audience }) {
+    try {
+      return await backend.gclient._orig_verifyIdToken({ idToken, audience })
+    } catch (e) {
+      console.log('hooking google auth verifyIdToken() function')
+      if (e.toString().includes('Error: Token used too late')) {
+        const loginTicket = new LoginTicket(ticket.envelope, ticket.payload)
+        return loginTicket
+      }
+    }
+  }
+  backend.gclient.verifyIdToken = verifyFn
 }
 
 const port = process.argv[2]
@@ -26,6 +43,11 @@ const backend = new Backend(
     audience: '202746986880-u17rbgo95h7ja4fghikietupjknd1bln.apps.googleusercontent.com',
     ecdsaKeyPair: keypair
   })
+
+if (process.argv[3] === '--dev') {
+  console.log('Running server in dev mode')
+  hookBackend(backend)
+}
 
 const server = new Webserver({ port, backend })
 server.start()
