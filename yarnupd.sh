169e9c30a11b1c316a232956d68aa54fd69e37dd
Dror Tirosh <dror@tabookey.com>
2019-12-26 15:40:18 +0200

    faster circle build
    - faster yarn install, using ./yarnupd.sh
    - removed unused packages from solidity/package.json
    - abort of description contains DRAFT (like this one...)
    note: currently, aborts AFTER yarn install. should abort before

---
+++ yarnupd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe
+#update yarn links it subfolders:
+root=`cd dirname $0;pwd`
+
+PKG=$root/sample/package.json
+
+cd $root/solidity && yarn && ( yarn unlink || echo ) && yarn link
+
+#remove current dependency on safechannels-contract... we add it as link in a minute..
+perl -pi.tmp -e 's/.*safechannels-contracts.*//' $PKG
+
+cd $root/sample && yarn link safechannels-contracts && yarn
+
+mv $PKG.tmp $PKG
+
+cd $root
+node solidity/extract_abi.js
+
